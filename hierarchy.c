66da32c08e24977a0eac1dfcfa18a24b0f828b4c
ac0910 <118996679+ac0910@users.noreply.github.com>
2023-09-09 10:55:38 +0530

    Add files via upload

---
+++ hierarchy.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i=0;
+    int k=2;
+    float a=0;
+    float b=2;
+    printf("%d %d %f %f",i , k , a , b);
+    return 0;
+    getch();
+}
